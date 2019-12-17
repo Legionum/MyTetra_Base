8d315339e107a85154db2ab1f09833356c31f4b1
Legionum <legionim@yandex.ru>
2019-12-18 02:06:55 +0300

    MyTetraCommit

--- base/15765919301a3qftgnnz/15766238229y25dmujjj.sh
+++ base/15765919301a3qftgnnz/15766238229y25dmujjj.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-
+#В данном скрипет используется функция kdialog
+#очень полезная штука при работе в среде KDE
+#данный экземпляр получает существующую директорию
+#нужно выбрать нужную папку, в которой нужно создать три
+#подпапки для проектов 3д-редактора и нажать "ОК"
 dir_1=$(kdialog --getexistingdirectory *);
-
+#диалоговые окна
 if [ "$?" = 0 ]; then
 	kdialog --msgbox "$dir_1";
 elif [ "$?" = 1 ]; then
@@ -9,7 +13,7 @@ elif [ "$?" = 1 ]; then
 else
 	kdialog --error "ERROR";
 fi;
-
+#создание директорий в выбранной через kdialog папке
 #konsole -e mkdir $dir_1'/textures' $dir_1'/models' $dir_1'/other'
 mkdir $dir_1'/textures' $dir_1'/models' $dir_1'/other'
 exit 0
